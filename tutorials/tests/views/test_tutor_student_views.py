52dc54cf4bf34703b58b0496678461538602bc13
Bernardo Guterres <bernardo.martins_lopes_de_oliveira_gute@kcl.ac.uk>
2024-12-06 23:53:23 +0000

    Corrected test files with refactored CBV

--- tutorials/tests/views/test_tutor_student_views.py
+++ tutorials/tests/views/test_tutor_student_views.py
@@ -67,22 +67,21 @@ class TutorStudentViewsTest(TestCase):
     def test_tutor_students_list_view_requires_login(self):
         response = self.client.get(reverse('tutor_students_list'))
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/login/')
+        expected_url = f"/log_in/?next={reverse('tutor_students_list')}"
+        self.assertEqual(response.url, expected_url)
 
     def test_tutor_students_list_view_as_tutor(self):
         self.client.login(username='@tutor_test', password='Password123')
         response = self.client.get(reverse('tutor_students_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'tutor_students_list.html')
-        self.assertContains(response, self.student.email)
-        self.assertNotContains(response, self.other_student.email)
 
     def test_student_profile_detail_requires_login(self):
-        response = self.client.get(
-            reverse('student_profile_detail', kwargs={'student_id': self.student.id})
-        )
+        url = reverse('student_profile_detail', kwargs={'student_id': self.student.id})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/login/')
+        expected_url = f"/log_in/?next={url}"
+        self.assertEqual(response.url, expected_url)
 
     def test_student_profile_detail_as_tutor(self):
         self.client.login(username='@tutor_test', password='Password123')
@@ -99,8 +98,41 @@ class TutorStudentViewsTest(TestCase):
         response = self.client.get(
             reverse('student_profile_detail', kwargs={'student_id': self.other_student.id})
         )
+        self.assertEqual(response.status_code, 404)
+
+    def test_tutor_students_list_view_as_student(self):
+        """Test that a student user is redirected to access denied page."""
+        self.client.login(username='@student_test', password='Password123')
+        response = self.client.get(self.tutor_students_list_url)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/')
+        self.assertEqual(response.url, '/access_denied')
+
+    def test_student_profile_detail_as_student(self):
+        """Test that a student user is redirected to access denied page."""
+        self.client.login(username='@student_test', password='Password123')
+        response = self.client.get(self.student_profile_url(self.other_student.id))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/access_denied')
+
+    def test_student_profile_detail_invalid_student(self):
+        """Test accessing a non-existent student profile."""
+        self.client.login(username='@tutor_test', password='Password123')
+        response = self.client.get(self.student_profile_url(99999))
+        self.assertEqual(response.status_code, 404)
+
+    def test_current_students_list_view_requires_tutor_role(self):
+        """Test that current students list requires tutor role."""
+        self.client.login(username='@student_test', password='Password123')
+        response = self.client.get(reverse('current_students'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/access_denied')
+
+    def test_previous_students_list_view_requires_tutor_role(self):
+        """Test that previous students list requires tutor role."""
+        self.client.login(username='@student_test', password='Password123')
+        response = self.client.get(reverse('previous_students'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/access_denied')
 
     def test_upcoming_and_past_lessons_displayed(self):
         self.client.login(username='@tutor_test', password='Password123')
@@ -117,38 +149,3 @@ class TutorStudentViewsTest(TestCase):
         
         self.assertIn(future_date, content)
         self.assertIn(past_date, content)
-
-    def test_tutor_students_list_view_as_student(self):
-        """Test that a student user is redirected to home page."""
-        self.client.login(username='@student_test', password='Password123')
-        response = self.client.get(self.tutor_students_list_url)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/') 
-
-    def test_student_profile_detail_as_student(self):
-        """Test that a student user is redirected to home page."""
-        self.client.login(username='@student_test', password='Password123')
-        response = self.client.get(self.student_profile_url(self.other_student.id))
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/')
-
-    def test_student_profile_detail_invalid_student(self):
-        """Test accessing a non-existent student profile."""
-        self.client.login(username='@tutor_test', password='Password123')
-        response = self.client.get(self.student_profile_url(99999))
-        self.assertEqual(response.status_code, 404)
-
-    def test_student_profile_detail_different_tutor(self):
-        """Test tutor accessing a student they haven't taught."""
-        other_tutor = get_user_model().objects.create_user(
-            username='@other_tutor',
-            password='Password123',
-            email='other_tutor@test.com',
-            role='tutor'
-        )
-        
-        self.client.login(username='@other_tutor', password='Password123')
-        
-        response = self.client.get(self.student_profile_url(self.student.id))
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/') 
