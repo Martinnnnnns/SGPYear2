38bc7403b7e5a92bcd11e14861ed5ed5f263eb1d
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2024-12-13 14:54:51 +0000

    Implement django admin panel

--- tutorials/admin.py
+++ tutorials/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from tutorials.models import Role, User, ProgrammingLanguage, Subject, Lesson, LessonRequest, Invoice, TutorAvailability, CancellationRequest, ChangeRequest
 
-# Register your models here.
+#Registering models .
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(ProgrammingLanguage)
+admin.site.register(Subject)
+admin.site.register(Lesson)
+admin.site.register(LessonRequest)
+admin.site.register(TutorAvailability)
+admin.site.register(ChangeRequest)
+admin.site.register(CancellationRequest)
+admin.site.register(Invoice)
